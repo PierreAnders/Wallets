21e7a5eed2d9f94eaa2cf25d8cfceda15030633b
pierreuntas <pierre.untas@gmail.com>
2023-02-01 16:01:51 +0100

    add select 1000 cryptos

--- app/models/crypto.rb
+++ app/models/crypto.rb
@@ -7,10 +7,27 @@ class Crypto < ApplicationRecord
 
   require 'net/http'
   require 'json'
-  @url = 'https://api.coingecko.com/api/v3/coins/markets?vs_currency=eur&order=market_cap_desc&per_page=250'
-  @uri = URI(@url)
-  @response = Net::HTTP.get(@uri)
-  @search_cryptos = JSON.parse(@response)
+  @url1 = 'https://api.coingecko.com/api/v3/coins/markets?vs_currency=eur&order=market_cap_desc&per_page=250&page=1'
+  @uri1 = URI(@url1)
+  @response1 = Net::HTTP.get(@uri1)
+  @search_cryptos1 = JSON.parse(@response1)
+
+  @url2 = 'https://api.coingecko.com/api/v3/coins/markets?vs_currency=eur&order=market_cap_desc&per_page=250&page=2'
+  @uri2 = URI(@url2)
+  @response2 = Net::HTTP.get(@uri2)
+  @search_cryptos2 = JSON.parse(@response2)
+
+  @url3 = 'https://api.coingecko.com/api/v3/coins/markets?vs_currency=eur&order=market_cap_desc&per_page=250&page=3'
+  @uri3 = URI(@url3)
+  @response3 = Net::HTTP.get(@uri3)
+  @search_cryptos3 = JSON.parse(@response3)
+
+  @url4 = 'https://api.coingecko.com/api/v3/coins/markets?vs_currency=eur&order=market_cap_desc&per_page=250&page=4'
+  @uri4 = URI(@url4)
+  @response4 = Net::HTTP.get(@uri4)
+  @search_cryptos4 = JSON.parse(@response4)
+
+  @search_cryptos = @search_cryptos1 + @search_cryptos2 + @search_cryptos3 + @search_cryptos4
 
   @cryptos_option = []
   @search_cryptos.each do |search_crypto|
