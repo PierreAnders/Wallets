2cc1e225bab4f6c2948576ad5499abfabf0aaf45
pierreuntas <pierre.untas@gmail.com>
2023-02-06 16:46:47 +0100

    clean index transfers

--- app/controllers/transfers_controller.rb
+++ app/controllers/transfers_controller.rb
@@ -1,9 +1,18 @@
 class TransfersController < ApplicationController
+  require 'httparty'
 
   def new
   end
 
   def index
     @wallets = Wallet.where(user: current_user)
+    response = HTTParty.get("https://api.etherscan.io/api?module=account&action=txlist&address=#{params[:query]}&startblock=0&endblock=99999999&sort=asc&apikey=BBEVJRYJKR3QDT95XU2QM686YEGXM7XNN3")
+    @transactions = response.parsed_response['result']
+    @address_option = []
+    @wallets.each do |wallet|
+      wallet.cryptos.each do |crypto|
+        @address_option.push(crypto.address) if crypto.name == "Ethereum"
+      end
+    end
   end
 end
