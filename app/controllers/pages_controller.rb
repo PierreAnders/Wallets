0791bfe663a238ece7285688f60f41bab20bdae6
PierreAnders <pierre.untas@gmail.com>
2023-05-24 00:37:57 +0200

    api crypto service

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,34 +3,12 @@ class PagesController < ApplicationController
   require 'json'
 
   def home
-    @url1 = 'https://api.coingecko.com/api/v3/coins/markets?vs_currency=eur&order=market_cap_desc&per_page=100&page=1'
-    @uri1 = URI(@url1)
-    @response1 = Net::HTTP.get(@uri1)
-    @coins1 = JSON.parse(@response1)
-
-    # @url2 = 'https://api.coingecko.com/api/v3/coins/markets?vs_currency=eur&order=market_cap_desc&per_page=250&page=2'
-    # @uri2 = URI(@url2)
-    # @response2 = Net::HTTP.get(@uri2)
-    # @coins2 = JSON.parse(@response2)
-
-    @coins = @coins1
-    # + @coins2
+    @coins = CoinGeckoService.new.get_coins_markets
+    @select_cryptos = @coins.map { |coin| coin["name"] }.sort
   end
 
   def portfolio
-
-    @url1 = 'https://api.coingecko.com/api/v3/coins/markets?vs_currency=eur&order=market_cap_desc&per_page=100&page=1'
-    @uri1 = URI(@url1)
-    @response1 = Net::HTTP.get(@uri1)
-    @search_cryptos1 = JSON.parse(@response1)
-
-    # @url2 = 'https://api.coingecko.com/api/v3/coins/markets?vs_currency=eur&order=market_cap_desc&per_page=250&page=2'
-    # @uri2 = URI(@url2)
-    # @response2 = Net::HTTP.get(@uri2)
-    # @search_cryptos2 = JSON.parse(@response2)
-
-    @search_cryptos = @search_cryptos1 
-    # + @search_cryptos2
+    @search_cryptos = CoinGeckoService.new.get_coins_markets
 
     @wallets = Wallet.where(user: current_user)
 
@@ -44,13 +22,13 @@ class PagesController < ApplicationController
       wallet.cryptos.sort_by(&:name).each do |crypto|
         @search_crypto = @search_cryptos.find { |search_crypto| search_crypto["name"] == crypto.name }
         @crypto_current_price = @search_crypto["current_price"]
-        @crypto_value = crypto.number * @crypto_current_price
+        @crypto_value = crypto.number.to_f * @crypto_current_price.to_f
         @portfolio_value += @crypto_value
       end
 
       wallet.cryptos.sort_by(&:name).each do |crypto|
         @search_crypto = @search_cryptos.find { |search_crypto| search_crypto["name"] == crypto.name }
-        @crypto_current_price = @search_crypto["current_price"]
+        @crypto_value = crypto.number.to_f * @crypto_current_price.to_f
         @crypto_change_24h = @search_crypto["price_change_percentage_24h"]
         @crypto_symbol = @search_crypto["symbol"]
         @crypto_logo = @search_crypto["image"]
@@ -86,4 +64,16 @@ class PagesController < ApplicationController
 
     @wallets = Wallet.where(user: current_user)
   end
+
+  def fetch_coingecko_data
+    Rails.cache.fetch('coingecko_data', expires_in: 10.minutes) do
+      url = 'https://api.coingecko.com/api/v3/coins/markets?vs_currency=eur&order=market_cap_desc&per_page=50&page=1'
+      uri = URI(url)
+      response = Net::HTTP.get(uri)
+      JSON.parse(response)
+    end
+  rescue => e
+    Rails.logger.error "Failed to fetch data from Coingecko: #{e.message}"
+    []
+  end  
 end
--- app/controllers/wallets_controller.rb
+++ app/controllers/wallets_controller.rb
@@ -22,7 +22,7 @@ class WalletsController < ApplicationController
   def show
     @crypto = Crypto.new
     @wallet = Wallet.find(params[:id])
-
+    @select_cryptos = CoinGeckoService.new.search_for_crypto.map { |coin| coin["name"] }.sort
     @wallet_value = 0
     @number_of_crypto = @wallet.cryptos.size
     @wallet_value_change_24h = 0
@@ -85,20 +85,6 @@ class WalletsController < ApplicationController
   end
 
   def search_for_crypto
-    require 'net/http'
-    require 'json'
-
-    @url1 = 'https://api.coingecko.com/api/v3/coins/markets?vs_currency=eur&order=market_cap_desc&per_page=250&page=1'
-    @uri1 = URI(@url1)
-    @response1 = Net::HTTP.get(@uri1)
-    @search_cryptos1 = JSON.parse(@response1)
-
-    # @url2 = 'https://api.coingecko.com/api/v3/coins/markets?vs_currency=eur&order=market_cap_desc&per_page=250&page=2'
-    # @uri2 = URI(@url2)
-    # @response2 = Net::HTTP.get(@uri2)
-    # @search_cryptos2 = JSON.parse(@response2)
-
-    @search_cryptos = @search_cryptos1
-    # + @search_cryptos2
+    @search_cryptos = CoinGeckoService.new.search_for_crypto
   end
 end
